b7cec7fb1457f8e00028537978c28a9d41422276
cherylrobles <134238929+cherylrobles@users.noreply.github.com>
2025-03-01 02:15:17 -0600

    Control de citas Vetcare
    Creación del proyecto / Vistas del usuario.

---
+++ src/test/java/com/Vetcare/VetcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vetcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
